04166c381cd06ef524f0cecd25c299a71b77eb78
Subhadeep Malik <subhadeepmalik004@gmail.com.com>
2024-05-02 08:50:44 +0530

    first commit

---
+++ FlotDataType.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    float x = 3.14;
+    printf("%d\n",x);// %d -->> int
+    printf("%f\n",x);// %f -->> float
+    return 0;
+}
---
+++ Variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int x = 5;
+    printf("%d",x);// %d -->> format pasifire
+    printf("\n");
+    x = 7;
+    printf("%d",x);
+    printf("\n");
+    x = x + 6;
+    printf("%d",x);
+    printf("\n");
+    x = x - 20;
+    printf("%d",x);
+    return 0;
+}
